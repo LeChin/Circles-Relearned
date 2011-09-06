fb96191082d9695c46e33890604ca87156cda757
Nichole Barrett <elohcin@gmail.com>
2011-09-06 16:14:13 -0700

    newest circle explosion

--- circle.js
+++ circle.js
@@ -1,38 +1,37 @@
 var circleLand = function(){
 	//gets canvas element from html, so we can use it
-	var canvas; 
-	var guy; 
+	var canvas;
+	var guy;
 	
 	
-	var circles = [];  //makes an array called circles  
+	var shapes = [];
 	var square;
-			
+	
 	var randomPosition = function(){
 		return 10 + Math.floor(Math.random() * 491);
 	};
 	
 	//this function stores the circle variable and all the elements needed to draw a random circle
-	var makeCircle = function(x,y){ 
-	    var circle = {
-	    	x: x, 
-	    	y: y,
-	    	dx: (Math.random() * 10) - 5, //negative number adds other directions
-	    	dy: (Math.random() * 10) - 5,
-	    	radius: 10 + Math.floor(Math.random() * 100),
-	    	color: "rgb(" + Math.floor(Math.random() * 55)+ "," + Math.floor(Math.random() * 155)+ "," + Math.floor(Math.random() * 255) + ")",
-	    	draw: function(){ //moved this from independent function, to part of object.
+	var makeCircle = function(x,y){
+		var circle = {
+			x: x,
+			y: y,
+			dx: (Math.random() * 10) - 5, //negative number adds other directions
+			dy: (Math.random() * 10) - 5,
+			radius: 10 + Math.floor(Math.random() * 10),
+			color: "rgb(" + Math.floor(Math.random() * 5)+ "," + Math.floor(Math.random() * 5)+ "," + Math.floor(Math.random() * 255) + ")",					draw: function(){ //moved this from independent function, to part of object.
 				guy.fillStyle = this.color;
 				guy.beginPath();
 				guy.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
 				guy.closePath();
 				guy.fill();
-	    	},
-	    	move: function(){
-	    		this.x += this.dx;
-	    		this.y += this.dy;
-	    	}
-	    };
-	    return circle; //returns the circle variable, need this to see what we've done
+			},
+			move: function(){
+				this.x += this.dx;
+				this.y += this.dy;
+			}
+		};
+		return circle; //returns the circle variable, need this to see what we've done
 	};
 	
 	
@@ -44,106 +43,98 @@ var circleLand = function(){
 			h: h,
 			theta: 0.0,
 			dtheta: Math.random(),
-			//dx: (Math.random() * 20) - 10,
-			//dy: (Math.random() * 20) - 10,
-			color: "rgb(" + Math.floor(Math.random() * 255)+ "," + Math.floor(Math.random() * 55) + "," + Math.floor(Math.random() * 255) + ")",
+			color: "rgb(" + Math.floor(Math.random() * 255)+ "," + Math.floor(Math.random() * 0) + "," + Math.floor(Math.random() * 255) + ")",
 			draw: function(){
 				//guy.clearRect(700,900,700,900)
 				guy.save();
 				guy.translate(this.x, this.y);
 				guy.rotate(this.theta);
 				guy.fillStyle = this.color;
-			guy.fillRect(-this.w / 2, -this.h / 2, this.w, this.h);
-			guy.restore();
+				guy.fillRect(-this.w / 2, -this.h / 2, this.w, this.h);
+				guy.restore();
 			},
 			move: function(){
 				this.theta += this.dtheta;
 			}
 		};
-		
-	    console.log("square");
+		console.log("square");
 		return square;
 	};
 	
 	
-	square = makeSquare(600, 100, 50, 50);
+	square = makeSquare(randomPosition(), randomPosition(), 50, 50);
 	
-	
-	var makeAllCircles = function(){
-		for (var cc = 0; cc < 7; cc++){
-			circles.push(makeCircle(randomPosition(), randomPosition()));
-	    }
+	var makeAllShapes = function(){
+		for (var i = 0; i < 100; i++){	
+			shapes.push(makeCircle(230, 220), makeSquare(randomPosition(), randomPosition(), 25, 25));
+		}	
 	};
 	
-	var drawAllCircles = function() {
+	var drawAllShapes = function() {
 		clear();
-	    for (var cc = 0; cc < circles.length; cc++) {
-	    	var circle = circles[cc];
-	        circle.draw();
-	    }
-	    //var square = makeSquare(800, 100, 100, 100); //techno chocolate
-		square.draw();
-		square.move();
+		for (var i = 0; i < shapes.length; i++) {
+			var shape = shapes[i];
+			shape.draw();
+		}
 	};
 	
-	var moveAllCircles = function(){
-		for (var cc = 0; cc < circles.length; cc++){
-			circles[cc].move();	
+	var moveAllShapes = function(){
+		for (var cc = 0; cc < shapes.length; cc++){
+			shapes[cc].move();
 		}
 	};
 	
 	var startCircles = function(circle){
-		var startMoving = function(){	
-			moveAllCircles();	
-			drawAllCircles();
+		var startMoving = function(){
+			moveAllShapes();
+			drawAllShapes();
 		};
-		
 		timerID = setInterval(startMoving, 50);
 	};
 	
 	
 	var clear = function() {
 		guy.clearRect(0, 0, canvas.width, canvas.height);
-		console.log("clearing!");	
+		console.log("clearing!");
 	};
-		
+	
 	
 	//when the window loads, the functions that are called execute
-	var init = function() { 
+	var init = function() {
 		canvas = document.getElementById('circles');
 		guy = canvas.getContext('2d');
 		
-		makeAllCircles();
-	    drawAllCircles();
+		makeAllShapes();
+		drawAllShapes();
 		
 		document.getElementById("move").onclick = function(e) {
-			moveAllCircles();
-			drawAllCircles();
+			moveAllShapes();
+			drawAllShapes();
 		};
 		
 		document.getElementById("start").onclick = function(e) {
 			startCircles();
-		};	
+		};
 		
 		document.getElementById("stop").onclick = function(e) {
 			clearInterval(timerID);
 		};
 		
 		document.getElementById("reset").onclick = function(e) {
-			circles = []; //clears circles from list
-			drawAllCircles(); //clears circles from this function
+			shapes = []; //clears circles from list
+			drawAllShapes(); //clears circles from this function
 		};
 		
 		document.getElementById("circles").onclick = function(e) {
-	         console.log(e);
-	    	 circles.push(makeCircle(e.layerX, e.layerY)); //offsetX/Y doesn't work in Firefox
-	
-	         drawAllCircles();
+			console.log(e);
+			shapes.push(makeCircle(e.layerX, e.layerY)/*, makeSquare(e.layerX, e.layerY, 70, 70)*/); //offsetX/Y doesn't work in Firefox
+			
+			drawAllShapes();
 		};
 	
 	};
 	
 	return {
-		init: init	
+		init: init
 	};
-}();             
+}(); 
